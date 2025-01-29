e4c71150bc5185c79e3775b72e05f2bebe84c654
Andrejs Ivanovs DP3-1 <ai00251@rvt.lv>
2025-01-29 13:50:29 +0200

    29.01

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,49 +1,54 @@
 <template>
-    <nav>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#groups">Groups</a></li>
-        <li><a href="#crew">Crew</a></li>
-        <li><a href="#apparel">Apparel & Gift Cards</a></li>
-        <li><a href="#events">Events</a></li>
-        <li><a href="#auditions">Auditions</a></li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#groups">Groups</a></li>
+          <li><a href="#crew">Crew</a></li>
+          <li><a href="#apparel">Apparel & Gift Cards</a></li>
+          <li><a href="#events">Events</a></li>
+          <li><a href="#auditions">Auditions</a></li>
+        </ul>
+      </nav>
+    </header>
   </template>
   
   <script setup>
-
+  // No special logic for now
   </script>
   
   <style scoped>
+  @font-face {
+    font-family: 'NeueHaasDisplay';
+    src: url('/fonts/NeueHaasDisplayMedium.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  
+  header {
+    position: absolute;
+    top: 3%;
+    left: 25%; 
+    width: 90%;
+    z-index: 10; 
+  }
 
-@font-face {
-  font-family: 'NeueHaasDisplay';
-  src: url('/fonts/NeueHaasDisplayMedium.ttf') format('truetype');
-  font-weight: normal;
-  font-style: normal;
-}
-
-header {
-  position: absolute;
-  top: 0;
-  left: 15%;
-  width: 90%;
-}
-
-nav {
-  font-family: 'NeueHaasDisplay', sans-serif; 
-  color: #fff;
-  font-size: 3rem; 
-  padding: 20px 0;
-}
-
-nav ul {
-  display: flex;
-  gap: 50px;
-  list-style-type: none;
-  justify-content: flex-start;
-}
-
-</style>
+  nav a {
+    font-family: 'NeueHaasDisplay', sans-serif;
+    color: #ffffff;
+    font-size: 2.5rem; /* Simulate zoom */
+    padding: 20px 0;
+  }
+  
+  nav ul {
+    display: flex;
+    gap: 50px;
+    list-style-type: none;
+    justify-content: flex-start;
+  }
+  
+  nav a:hover {
+    color: #ffe08a; 
+  }
+  </style>
   
